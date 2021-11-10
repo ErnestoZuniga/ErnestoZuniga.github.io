695b814669297dd2b5bba47d1cea906b6eb3f0df
Ernesto Zuñiga <wandern.dechecs@gmail.com>
2021-11-09 18:46:36 -0600

    App.js configured to work with react-router-dom. Structured folders and moved files of project for better organization

---
+++ src/routes/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "../containers/Home"
+
+
+const App = () => {
+return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
+    </Router>
+);}
+
+export default App;
