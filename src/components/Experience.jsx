d5e90103f714330bf89089206df6d87e736c4274
ErnestoZuniga <wandern.dechecs@gmail.com>
2021-11-12 23:54:34 -0600

    Feature: added Experience.jsx component with working functionality

---
+++ src/components/Experience.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import * as BsIcons from 'react-icons/bs';
+
+const Experience = (props) => {
+  return(
+    <div>
+      <h2>Experiencia</h2>
+      <h3>{props.position}</h3>
+      <h4>{props.company}</h4>
+      <section>
+        <div>
+          <BsIcons.BsCalendar2Date/>
+          <p>{props.dateStart} - {props.dateEnd}</p>
+        </div>
+        <div>
+          <BsIcons.BsGeoAltFill/> 
+          <p>{props.workPlace}</p>
+        </div>
+      </section>
+      <section /*description*/ >
+        <p>{props.description}</p>
+      </section>
+      <div /*style lista*/ >
+        <ul>
+          {props.activities.map( (activity) => <li>{activity}</li>)}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Experience;
