1aadd1defade752c4ecf491b00d1ed0868525609
Ernesto Zuniga <wandern.dechecs@gmail.com>
2021-11-23 01:10:33 -0600

    Removed old inline styles and wrong classnames

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,48 +4,31 @@ import ContactInfo from './ContactInfo.jsx'
 
 
 const Header = () => {
-  const styles= {
-    'header-container':{
-      width:'100vw',
-      height:'30vh'
-    },
-    'contactinfo-container':{
-      display: 'flex',
-      flexWrap:'wrap',
-      justifyContent: 'space-around',
-      flexGrow: '1'
-    },
-    'img-container':{
-      width:'5rem',
-      height:'5rem'
-    }
-  }
 
   return (
-    <div style={styles['header-container']}>
     <div>
-      <h1>Luis Ernesto Zu&#241;iga Ontiveros</h1>
+      <img src={'https://i.imgur.com/2oKnRZ5.png'} alt="Ernesto"/>
+      <div>
+        <h1>Luis Ernesto Zu&#241;iga Ontiveros</h1>
+        <section>
+          <p>Front End Developer</p>
+          <p>Mechatronics Engineering</p>
+        </section>
+        <section>
+          <ContactInfo tel={'5518137358'} /> 
+          <ContactInfo email={'poli.ernesto95@gmail.com'} /> 
+          <ContactInfo addres={'Gustavo A. Madero, CDMX'}/> 
+          <ContactInfo facebook={'Facebook.com/Kamewalker'}/> 
+          <ContactInfo twitter={'@Kamewalker_Tr'}/>
+          <ContactInfo github={'github.com/ErnestoZuniga'}/>  
+          <ContactInfo linkedin={'linkedin'}/>  
+        </section>
+      </div>
       <section>
-        <p>Front End Developer</p>
-        <p>Mechatronics Engineering</p>
+        <button> Eng </button>
+        <button> Esp </button>
       </section>
-      <section style={styles['contactinfo-container']}>
-        
-        <ContactInfo tel={'5518137358'} /> 
-        <ContactInfo email={'poli.ernesto95@gmail.com'} /> 
-        <ContactInfo addres={'Gustavo A. Madero, CDMX'}/> 
-        <ContactInfo facebook={'Facebook.com/Kamewalker'}/> 
-        <ContactInfo twitter={'@Kamewalker_Tr'}/>
-        <ContactInfo github={'github.com/ErnestoZuniga'}/>  
-        <ContactInfo linkedin={'linkedin'}/>  
-      </section>
-    </div>
-    <img style={styles['img-container']} src={'https://i.imgur.com/2oKnRZ5.png'} alt="Ernesto"/>
-    <section>
-      <button> Eng </button>
-      <button> Esp </button>
-    </section>
-    </div>
+      </div>
   );
 }
 
--- src/containers/FrontEnd.jsx
+++ src/containers/FrontEnd.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-//import '../assets/styles/App.css';
 import Experience from '../components/Experience';
 import Proyects from '../components/Proyects';
 import LifePhilosophy from '../components/LifePhilosophy';
@@ -9,18 +8,8 @@ import Skills from '../components/Skills';
 const Home = () => {
   const description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere magna a quam posuere tempus. Donec nulla ex, venenatis quis ultrices a, varius sit amet felis. Nulla ultricies, magna eu feugiat malesuada, justo nisi fermentum massa, a porttitor sem tortor quis velit. Nulla non mauris suscipit, tempus nisl sed, varius nulla. In fermentum blandit tempor. Aliquam in nulla sed enim tincidunt volutpat. Sed et massa dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam eu quam neque. Nulla non risus lacus. Suspendisse dolor leo, vulputate sit amet mauris a, interdum commodo felis. Donec tempus quam sit amet ligula condimentum, at dictum eros efficitur.'
 
-  const styles= {
-    'main-container':{
-      width:'100vw',
-      height:'70vh',
-      display: 'flex',
-      flexWrap:'wrap',
-      justifyContent: 'space-around',
-      flexGrow: '1'
-    }
-  }
   return (
-  <div style={styles['main-container']}>
+  <div>
     <Experience
       title={true}
       position={'FrontEnd Developer'}
