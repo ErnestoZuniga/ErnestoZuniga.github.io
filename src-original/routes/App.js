3f5eec9ff7d97b7c02a7284beefeae63f24e4013
Ernesto Zuniga <wandern.dechecs@gmail.com>
2022-03-01 20:29:53 -0600

    refactor: -New folder structure.

---
+++ src-original/routes/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
+
+import FrontEnd from "../containers/FrontEnd"
+import Mechatronics from "../containers/Mechatronics ";
+import All from "../containers/All";
+import NotFound from "../containers/NotFound"
+import Layout from "../components/Layout";
+import Playground from "../containers/Playground";
+
+
+
+const App = () => {
+  return (
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact={true} path="/" element={<All/>} />
+          <Route exact={true} path="/frontend" element={<FrontEnd/>}>\
+            { /*<Route path=':lang' element={<FrontEnd />} />*/}
+          </Route>
+          <Route exact={true} path="/mechatronics" element={<Mechatronics/>} />
+          <Route exact={true} path="*" element={<NotFound/>} />
+          <Route exact={true} path="/p" element={<Playground/>} />
+        </Switch>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
--- src/routes/App.js
+++ src/routes/App.js
@@ -5,29 +5,35 @@ import {
   Route,
 } from "react-router-dom";
 
-import FrontEnd from "../containers/FrontEnd"
-import Mechatronics from "../containers/Mechatronics ";
-import All from "../containers/All";
-import NotFound from "../containers/NotFound"
-import Layout from "../components/Layout";
-import Playground from "../containers/Playground";
+// import FrontEnd from "../containers/FrontEnd"
+// import Mechatronics from "../containers/Mechatronics ";
+// import All from "../containers/All";
+// import NotFound from "../containers/NotFound"
+// import Layout from "../components/Layout";
+// import Playground from "../containers/Playground";
+import HomePage from "../pages/HomePage";
 
 
 
 const App = () => {
   return (
+    // <Router>
+    //   <Layout>
+    //     <Switch>
+    //       <Route exact={true} path="/" element={<All/>} />
+    //       <Route exact={true} path="/frontend" element={<FrontEnd/>}>\
+    //         { /*<Route path=':lang' element={<FrontEnd />} />*/}
+    //       </Route>
+    //       <Route exact={true} path="/mechatronics" element={<Mechatronics/>} />
+    //       <Route exact={true} path="*" element={<NotFound/>} />
+    //       <Route exact={true} path="/p" element={<Playground/>} />
+    //     </Switch>
+    //   </Layout>
+    // </Router>
     <Router>
-      <Layout>
-        <Switch>
-          <Route exact={true} path="/" element={<All/>} />
-          <Route exact={true} path="/frontend" element={<FrontEnd/>}>\
-            { /*<Route path=':lang' element={<FrontEnd />} />*/}
-          </Route>
-          <Route exact={true} path="/mechatronics" element={<Mechatronics/>} />
-          <Route exact={true} path="*" element={<NotFound/>} />
-          <Route exact={true} path="/p" element={<Playground/>} />
-        </Switch>
-      </Layout>
+      <Switch>
+        <Route exact path="/" element={<HomePage/>} /> 
+      </Switch>
     </Router>
   );
 }
