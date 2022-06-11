9e28ef37bb198f8a028a52a81b0862f65f70f8f3
Ernesto Zuniga <wandern.dechecs@gmail.com>
2022-06-11 13:56:12 -0500

    Feature: Added Loader component when first enter to site

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,16 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import { Header, layout } from "../config/resources";
+
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Layout = ({ children }) => {
   const styles = layout.main;
   const body = layout.body;
 
+  const [open,setOpen] = useState(false)
+
+  useEffect(() => {
+    handleToggleBackdrop();
+  },[]);
+
+  const handleToggleBackdrop = () => {
+    setOpen(true);
+    setTimeout(() => {
+      handleCloseBackdrop()
+    }, 750);
+  };
+
+  const handleCloseBackdrop = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className={styles}>
-      <Header />
-      <div className={body}>{children}</div>
-    </div>
+    <>
+      <div className={styles}>
+        <Header />
+        <div className={body}>
+          {children}
+        </div>
+      </div>
+
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={null}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
   );
 };
 
